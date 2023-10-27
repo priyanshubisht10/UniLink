cdacbeed68a958a36996cda00a630cc2a5abf5ff
Ujjwal0712 <119096131+Ujjwal0712@users.noreply.github.com>
2023-10-27 09:05:21 +0530

    Added Drawables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.unilink"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.unilink"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -58,6 +58,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/unilink/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/unilink/fragments/SearchFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.prog_o_thonproject.R
+
 import com.example.unilink.R
 
 class SearchFragment: Fragment() {
